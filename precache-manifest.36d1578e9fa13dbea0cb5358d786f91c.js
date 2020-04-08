843b608ec558dbecc1bbfb57d36a541ed6831d18
abdiqayum <isacad1920@gmail.com>
2020-04-08 15:32:34 +0300

    Updates

---
+++ precache-manifest.36d1578e9fa13dbea0cb5358d786f91c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbe1f4b597e2f74d2924efa5aa101cde",
+    "url": "/advice-app/index.html"
+  },
+  {
+    "revision": "5335ce32eae1bf1f9c0c",
+    "url": "/advice-app/static/css/2.007bafff.chunk.css"
+  },
+  {
+    "revision": "4aae11c667d9dc63b344",
+    "url": "/advice-app/static/css/main.afd23e73.chunk.css"
+  },
+  {
+    "revision": "5335ce32eae1bf1f9c0c",
+    "url": "/advice-app/static/js/2.516f7729.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/advice-app/static/js/2.516f7729.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4aae11c667d9dc63b344",
+    "url": "/advice-app/static/js/main.a717dc9f.chunk.js"
+  },
+  {
+    "revision": "59d4ddffa934e6e433d3",
+    "url": "/advice-app/static/js/runtime-main.a4d8b64a.js"
+  },
+  {
+    "revision": "2f0053a9bd591c79fbebe61820a5d5ac",
+    "url": "/advice-app/static/media/bg.2f0053a9.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/advice-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/advice-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/advice-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/advice-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/advice-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ibnusacad.github.io/advice-app/precache-manifest.a78a85751c4f80bf737bc22c0c0c41d7.js"
+  "/advice-app/precache-manifest.36d1578e9fa13dbea0cb5358d786f91c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ibnusacad.github.io/advice-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/advice-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
