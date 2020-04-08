d6c65f9fbc2a3933b52675937714d9c7c84b7e30
abdiqayum <isacad1920@gmail.com>
2020-04-08 15:27:01 +0300

    Updates

---
+++ precache-manifest.1e12be09e0d46567336c3c41618cf9c2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f53c1a26806a36f3e18a3c4da28831eb",
+    "url": "/ibnusacad/advice-app/index.html"
+  },
+  {
+    "revision": "5c2867401b16efd8ed06",
+    "url": "/ibnusacad/advice-app/static/css/2.e1f5724d.chunk.css"
+  },
+  {
+    "revision": "d6089acffbaf7d71523a",
+    "url": "/ibnusacad/advice-app/static/css/main.b696087e.chunk.css"
+  },
+  {
+    "revision": "5c2867401b16efd8ed06",
+    "url": "/ibnusacad/advice-app/static/js/2.516f7729.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ibnusacad/advice-app/static/js/2.516f7729.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6089acffbaf7d71523a",
+    "url": "/ibnusacad/advice-app/static/js/main.a717dc9f.chunk.js"
+  },
+  {
+    "revision": "78b44517e07104a9e108",
+    "url": "/ibnusacad/advice-app/static/js/runtime-main.b1cef371.js"
+  },
+  {
+    "revision": "2f0053a9bd591c79fbebe61820a5d5ac",
+    "url": "/ibnusacad/advice-app/static/media/bg.2f0053a9.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ibnusacad/advice-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ibnusacad/advice-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ibnusacad/advice-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ibnusacad/advice-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ibnusacad/advice-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
